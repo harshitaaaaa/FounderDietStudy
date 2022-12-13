2f121c77885ff6e5f48746f4d5f625a275ca64ee
Brian Yandell <byandell@wisc.edu>
2022-12-13 16:48:26 -0600

    add strain checkbox, strain/sex_diet swap

--- app.R
+++ app.R
@@ -2,7 +2,9 @@ library(shiny)
 library(tidyverse)
 library(readr)
 library(readxl)
-
+CCcolors <- c("#F0E442", "#555555", "#E69F00", "#0072B2",
+              "#56B4E9", "#009E73", "#D55E00", "#CC79A7")
+names(CCcolors) <- c("AJ", "B6", "129", "NOD", "NZO", "CAST", "PWK", "WSB")
 ################################################################
 
 
@@ -23,6 +25,8 @@ ui <- fluidPage(
             selectInput("order", "Order traits by",
                         c("p_sex_diet", "p_diet", "p_sex", "variability", "alphabetical", "original"),
                         "p_sex_diet"))),
+        radioButtons("facet","Facet by", c("strain","sex_diet"), "strain", inline = TRUE),
+        uiOutput("strains"),
         sliderInput("height", "Plot height (in):", 3, 10, 6, step = 1),
         uiOutput("downloadPlotUI"),
         fluidRow(
@@ -36,8 +40,8 @@ ui <- fluidPage(
     
     # Main panel for displaying outputs ----
     mainPanel(
-      uiOutput("outs")
-      
+      uiOutput("outs"),
+      uiOutput("scatPlot")
     )
   )
 )
@@ -57,6 +61,12 @@ server <- function(session, input, output) {
             "See",
             shiny::a("Attie Lab Diabetes Database", href = "http://diabetes.wisc.edu/"),
             "for earlier study.")
+  })
+  
+  output$strains <- renderUI({
+    choices <- names(CCcolors)
+    checkboxGroupInput("strains", "Strains",
+                       choices = choices, selected = choices, inline = TRUE)
   })
   
   # Trait summaries (for ordering traits, and summary table)
@@ -108,15 +118,19 @@ server <- function(session, input, output) {
   })
   
   # Data for selected traits
-  datatraits <- reactive({
-    req(dataset(), input$trait)
-    ltrait <- length(input$trait)
+  datatraitslong <- reactive({
+    req(dataset(), input$trait, input$strains)
     dataset() %>%
-      filter(trait %in% input$trait) %>%
+      filter(trait %in% input$trait,
+             strain %in% input$strains)
+  })
+  datatraits <- reactive({
+    req(datatraitslong(), input$trait)
+    ltrait <- length(input$trait)
+    datatraitslong() %>%
       mutate(trait = abbreviate(trait, ceiling(60 / ltrait))) %>%
       unite(sex_diet, sex, diet)
   })
-  
   
   # Output: Plots or Data
   output$outs <- shiny::renderUI({
@@ -143,10 +157,22 @@ server <- function(session, input, output) {
     }
     ltrait <- length(input$trait)
     
-    ggplot(datatraits()) +
-      aes(sex_diet, value, col = sex_diet) +
+    req(input$facet)
+    p <- ggplot(datatraits())
+    switch(input$facet,
+           strain = {
+             p <- p +
+               aes(sex_diet, value, col = sex_diet) +
+               facet_grid(trait ~ strain, scales = "free_y")
+           },
+           sex_diet = {
+             p <- p +
+               aes(strain, value, col = strain) +
+               facet_grid(trait ~ sex_diet, scales = "free_y") +
+               scale_color_manual(values = CCcolors)
+           })
+    p +
       geom_jitter() +
-      facet_grid(trait ~ strain, scales = "free_y") +
       theme(legend.position = "none",
             axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
       ylab(ifelse(ltrait == 1, input$trait, "Trait Value")) +
@@ -224,6 +250,48 @@ server <- function(session, input, output) {
       utils::write.csv(traitarrange(), file, row.names = FALSE)
     }
   )
+  
+  output$pair <- renderUI({
+    req(input$trait)
+    if(length(input$trait) > 1) {
+      selectInput("pair", "Select pair for scatterplots",
+                  choices = input$trait, selected = input$trait[1:2],
+                  multiple = TRUE)
+    }
+  })
+  scatdata <- reactive({
+    req(input$trait, datatraitslong(), input$pair)
+    if(length(input$pair) < 2) {
+      return(NULL)
+    } else {
+      traitpair <- rep(input$pair, length = 2)
+    }
+    dat <- datatraitslong() %>%
+      filter(trait %in% traitpair) %>%
+      select(strain, number, sex, diet, trait, value) %>%
+      pivot_wider(names_from = "trait", values_from = "value")
+  })
+  output$scatPlot <- renderUI({
+    req(input$trait, input$datatype, input$order)
+    tagList(
+      uiOutput("pair"),
+      plotly::plotlyOutput("scatplot")
+    )
+  })
+  output$scatplot <- plotly::renderPlotly({
+    req(scatdata(), input$pair)
+    if(is.null(scatdata())) {
+      return(ggplot())
+    }
+    plotly::ggplotly(
+      ggplot(scatdata()) +
+        aes(.data[[input$pair[1]]], .data[[input$pair[2]]],
+            color = strain) +
+        geom_smooth(method = "lm", se = FALSE) +
+        geom_point(size = 2) +
+        scale_color_manual(values = CCcolors) +
+        facet_grid(sex ~ diet))
+  })
   
 }
 
