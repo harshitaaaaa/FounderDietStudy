845fc24c1a5dd1d8d4779ae7922707e12e4faef5
Brian Yandell <byandell@wisc.edu>
2023-03-21 18:48:30 -0500

    add ggdendro to app.R

--- app.R
+++ app.R
@@ -1,8 +1,9 @@
 library(shiny)
-devtools::install_cran("plotly") # plotly not yet on UW dataviz
-devtools::install_cran("markdown") # markdown not yet on UW dataviz
-devtools::install_cran("cowplot") # patchwork not yet on UW dataviz
-devtools::install_cran("patchwork") # patchwork not yet on UW dataviz
+devtools::install_cran("plotly") #  not yet on UW dataviz
+devtools::install_cran("markdown") #  not yet on UW dataviz
+devtools::install_cran("cowplot") #  not yet on UW dataviz
+devtools::install_cran("patchwork") #  not yet on UW dataviz
+devtools::install_cran("ggdendro") #  not yet on UW dataviz
 devtools::install_github("byandell/foundr")
 library(foundr)
 
