492ddf02ae0cdd8b366e49a7270030ca9060ce00
Brian Yandell <byandell@wisc.edu>
2023-03-31 13:17:22 -0500

    add skew Rmarkdown; study in Physio of AUC

--- R/Physio.R
+++ R/Physio.R
@@ -19,3 +19,43 @@ PhysioHarmony <- function(dataset, links, ...) {
     # These are harmonized columns and their names.
     select(strain, sex, animal, condition, trait, value)
 }
+
+PhysioGTTHarmony <- function(dataset, links, ...) {
+  # Compute GTT AUC for shucks.
+  out <- PhysioHarmony <- function(dataset, links, ...)
+  
+  # Add GTT area under curve traits
+  GTT <- out %>%
+    filter(grepl("GTT", trait)) %>%
+    separate_wider_delim(
+      trait,
+      delim = "_",
+      names = c("gtt","trait","minutes","week")) %>%
+    select(-gtt) %>%
+    area_under_curve() %>%
+    unite(trait, trait, week) %>%
+    mutate(trait = paste0("GTT_", trait)) %>%
+    select(strain, sex, animal, condition, trait, value)
+
+  bind_rows(out, GTT)
+}
+
+GTT_look <- function(dataset, links, ...) {
+  # Write a CSV file with various AUC calculations.
+  out <- PhysioGTTHarmony(dataset, links)
+  
+  out <- out %>%
+    filter(grepl("AUC", trait),
+           trait != "cpep_ins_molar_ratio_iAUC_14wk") %>%
+    mutate(trait = ifelse(grepl("GTT", trait),
+                          str_remove(trait, "^GTT_"),
+                          trait)) %>%
+    separate_wider_delim(
+      trait,
+      delim = "_",
+      names = c("trait","auc","week")) %>% 
+    pivot_wider(names_from = "auc", values_from = "value")
+  
+  write.csv(out, "auc.csv")
+  invisible(out)
+}
