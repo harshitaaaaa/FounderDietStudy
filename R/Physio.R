a6269ac78283a599dd5f1d8c654938b688f92372
Brian Yandell <byandell@wisc.edu>
2023-04-16 16:09:58 -0500

    change condition to diet; change KB_14wk to KetoneBodies

--- R/Physio.R
+++ R/Physio.R
@@ -4,6 +4,9 @@ PhysioHarmony <- function(dataset, links, ...) {
   # Data are in sheet 2 starting on line 1
   out <- read_excel(filename, sheet = 2) %>%
     
+    # Rename Ketone Bodies from KB_14wk
+    rename(KetoneBodies = "KB_14wk") %>%
+    
     # Traits begin in column 5
     pivot_longer(-(1:4), names_to = "trait", values_to = "value") %>%
     
@@ -18,21 +21,67 @@ PhysioHarmony <- function(dataset, links, ...) {
     
     # These are harmonized columns and their names.
     select(strain, sex, animal, condition, trait, value)
-  
-  # Add GTT area under curve traits
+
+  # Add area under curve traits for measurements over minutes.
   GTT <- out %>%
-    filter(grepl("GTT", trait)) %>%
+    filter(grepl("_[0-9]+_[0-9]+wk$", trait)) %>%
+    # Separate out minutes and week.
+    # Kludge to catch cpep ratio trait.
     separate_wider_delim(
       trait,
       delim = "_",
-      names = c("gtt","trait","minutes","week")) %>%
-    select(-gtt) %>%
-    area_under_curve() %>%
+      names = c("cpep1", "cpep2", "gtt","trait","minute","week"),
+      too_few = "align_end") %>%
+    mutate(trait = ifelse(
+      trait == "ratio",
+      paste(cpep1, cpep2, gtt, trait, sep = "_"),
+      paste(gtt, trait, sep = "_")))
+  
+  # Filter to traits with >1 minute measurement.
+  GTTct <- GTT %>%
+    distinct(trait, minute, week) %>%
+    count(trait, week) %>%
+    filter(n > 1)
+  
+  GTT <- GTT %>%
+    filter(trait %in% GTTct$trait & week %in% GTTct$week) %>%
+    # Calculate AUC and other summaries.
+    area_under_curve("minute") %>%
+    # Unite summary name with week.
     unite(trait, trait, week) %>%
-    mutate(trait = paste0("GTT_", trait)) %>%
+    # Harmonize names.
     select(strain, sex, animal, condition, trait, value)
 
-  bind_rows(out, GTT)
+  # Add area under curve traits for measurements over weeks.
+  wks <- out %>%
+    filter(grepl("_[0-9]+wk$", trait) & !grepl("_([0-9]+|tAUC|iAUC)_[0-9]+wk$", trait)) %>%
+    # Kludge to use AUC routine for now by calling weeks as minutes.
+    separate_wider_delim(
+      trait,
+      delim = "_",
+      names = c("trait1","trait","week"),
+      too_few = "align_end") %>%
+    mutate(
+      trait = ifelse(
+        is.na(trait1),
+        trait,
+        paste(trait1, trait, sep = "_")),
+      week = as.numeric(str_remove(week, "wk$")))
+  
+  # Filter to traits with >1 week measurement.
+  wksct <- wks %>%
+    distinct(trait, week) %>%
+    count(trait) %>%
+    filter(n > 1)
+  
+  wks <- wks %>%
+    filter(trait %in% wksct$trait) %>%
+    # Calculate AUC and other summaries.
+    area_under_curve("week") %>%
+    # Harmonize names.
+    select(strain, sex, animal, condition, trait, value)
+  
+  bind_rows(out, GTT, wks)
 }
 
 GTT_look <- function(dataset, links, ...) {
