ca8f442475c05c8e8d54cdacf5279972d37ce08b
Harshita Reddy Edugudi <harshita24899@gmail.com>
2023-08-02 12:32:18 -0500

    updated code

--- R/Lipid.R
+++ R/Lipid.R
@@ -1,4 +1,5 @@
 LiverLipHarmony <- function(dataset, annot, links, sheetNum, skipNum, minCol,maxCol,charCols,...,sampleKey) {
+
   # Get trait names
   traits <- unlist(read_excel(dataset, sheet = sheetNum,
                               skip = skipNum, n_max = 1, col_names = FALSE)[1,])
@@ -12,30 +13,28 @@ LiverLipHarmony <- function(dataset, annot, links, sheetNum, skipNum, minCol,max
   # Remove unwanted columns
   out <- out[, -c(minCol:maxCol)]
 
-  # adding mouse_id if absent
+  # Adding mouse_id if absent
   if (!missing(sampleKey)) {
-    #print("entered")
     out <- out%>%
-      left_join(sampleKey %>%
-                  select(`label on vial`, `sample name`),
-                by = c("Name" = "label on vial")) %>%
-      rename(mouse_id = `sample name`)
+           left_join(sampleKey %>%
+                        select(`label on vial`, `sample name`),
+                     by = c("Name" = "label on vial")) %>%
+           rename(mouse_id = `sample name`)
 
   }
 
-  # Add diet column
-  out <- left_join(
-    out,
-    annot %>%
-      rename(animal = "number", condition = "diet") %>%
-      select(mouse_id, strain, animal, sex, condition),
-    by = "mouse_id"
-  )
+  # Adding diet column
+  out <- left_join(out,
+                   annot %>%
+                      rename(animal = "number", condition = "diet") %>%
+                      select(mouse_id, strain, animal, sex, condition),
+                  by = "mouse_id")
 
-  # pivot to longer format
+  # Pivot to longer format
   out <- out %>%
-    pivot_longer(cols = !charCols,
-                 names_to = "lipids", values_to = "value")
+         pivot_longer(cols = !charCols,
+                      names_to = "lipids",
+                      values_to = "value")
 
 
 
