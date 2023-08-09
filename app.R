b9e7a0c0e8cc75810067c86abbc47e4eed09cab7
Brian Yandell <byandell@wisc.edu>
2023-08-09 12:31:59 -0500

    updated app

--- app.R
+++ app.R
@@ -1,22 +1,23 @@
-library(shiny)
 devtools::install_cran("plotly") #  not yet on UW dataviz
 devtools::install_cran("markdown") #  not yet on UW dataviz
 devtools::install_cran("cowplot") #  not yet on UW dataviz
 devtools::install_cran("ggdendro") #  not yet on UW dataviz
+#devtools::install_cran("RSQLite") #  not yet on UW dataviz
 devtools::install_github("byandell/foundr")
-library(foundr)
 
-harmonizeddir <- "/mnt/researchdrive/adattie/General/founder_diet_study/HarmonizedData"
-traitDir <- "Normalized" # "Unnormalized"
-#traitData <- readRDS(file.path(harmonizeddir, traitDir, "traitData.rds"))
-#traitStats <- readRDS(file.path(harmonizeddir, traitDir, "traitStats.rds"))
-#traitSignal <- readRDS(file.path(harmonizeddir, traitDir, "traitSignal.rds"))
-traitData <- readRDS(file.path(harmonizeddir, traitDir, "traitObject.rds"))
-traitStats <- traitSignal <- NULL
+dirpath <- file.path("big")
+traitData <- readRDS(file.path(dirpath, "traitData.rds"))
+traitSignal <- readRDS(file.path(dirpath, "traitSignal.rds"))
+traitStats <- readRDS(file.path(dirpath, "traitStats.rds"))
+
+#db <- RSQLite::dbConnect(RSQLite::SQLite(),
+#                         file.path(dirpath, "traitData.sqlite"))
+#traitData <- dplyr::tbl(db, "traitData")
 
 customSettings <- list(
   help = "help.md",
   condition = "diet",
+  entrykey = "Founder",
   dataset = c(
     PlaMet0 = "Plasma metabolites 0min",
     PlaMet120 = "Plasma metabolites 120min",
@@ -24,22 +25,24 @@ customSettings <- list(
     LivRna = "Liver gene expression",
     Physio = "Physiological traits",
     Enrich = "Plasma enrichment",
-    Module = "WGCNA Module eigentraits",
-    uploaded = "Uploaded"))
+    Module = "WGCNA Module eigentraits"))
 
 ################################################################
 
-ui <- foundr::foundrUI("Founder Diet Study")
+title <- "Founder Diet Study"
+
+ui <- foundr::ui(title)
 
 server <- function(input, output, session) {
-    
-  foundr::foundrServer(input, output, session,
-                       traitData, traitStats, traitSignal,
-                       customSettings)
+  
+  #  shiny::onStop(function() {RSQLite::dbDisconnect(db)})
+  
+  foundr::server(input, output, session,
+                 traitData, traitSignal, traitStats,
+                 customSettings)
   
   # Allow reconnect with Shiny Server.
   session$allowReconnect(TRUE)
 }
-
 
 shiny::shinyApp(ui = ui, server = server)
