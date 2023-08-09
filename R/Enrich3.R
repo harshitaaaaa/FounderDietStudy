f863dbb0fa4e1d8a87391b8710fe9c1cfbc87d6f
Brian Yandell <byandell@wisc.edu>
2023-08-09 16:57:32 -0500

    add enrichment and AUC traits to Enrich3

--- R/Enrich3.R
+++ R/Enrich3.R
@@ -2,58 +2,70 @@ Enrich3Harmony <- function(dataset, links, annot, ...) {
   filename <- linkpath(dataset, links)
   
   # Find all the sheets (3 of import).
-  all_sheets <- excel_sheets(filename)
+  all_sheets <- excel_sheets(filename)[-1]
   
   # Data processing for all sheets in the file except first.
-  processed_datasets <- 
-    lapply(all_sheets[-1], 
-           function(sheet) {
-             sheet_data <- read_excel(filename, sheet = sheet)
-             timepoint_data_cleaning(sheet_data)
-           })
-  
-  # Combine all the processed datasets into a single dataframe
-  combined_dataset <- do.call(rbind, processed_datasets)
-  
-  # Renaming Strains to mouse_id  
-  combined_dataset <- combined_dataset %>%
-    rename(mouse_id = Strains)
-  
+  combined_dataset <- list()
+  for(i in all_sheets) {
+    cat(i, "\n")
+    combined_dataset[[i]] <- read_trait_sheet(filename, i)
+  }
+  combined_dataset <- bind_rows(combined_dataset)
+
   # Adding strain, animal,sex and condition based on annotation file
-  left_join(
-    combined_dataset,
-    annot %>% 
-      rename(animal = "number", condition = "diet") %>% 
-      select(mouse_id,  strain, animal, sex, condition),
-    by = c("mouse_id")
-  ) %>%
-    rename(value = "NA Corrected with zero",
-           run = "Sample Name") %>%
-    mutate(animal = as.character(animal),
-           run = as.numeric(str_remove(str_remove(run, "-.*$"), "^run"))) %>%
+  out <- 
+    left_join(
+      combined_dataset,
+      annot %>% 
+        rename(animal = "number", condition = "diet") %>% 
+        select(mouse_id,  strain, animal, sex, condition),
+      by = c("mouse_id")) %>%
+    mutate(animal = as.character(animal)) %>%
     
     # Only use latest run if duplicates done.
+    select(strain, sex, animal, condition, trait, minute, value)
+  
+  # Add enrichment traits Mn
+  out <- out %>%
     group_by(strain, sex, animal, condition, trait) %>%
-    summarize(value = value[which.max(run)], .groups = "drop") %>%
+    mutate(mvalue = value / sum(value),
+           minute = minute) %>%
     ungroup() %>%
+    select(value, mvalue, everything()) %>%
+    pivot_longer(value:mvalue, names_to = "enrich", values_to = "value") %>%
+    mutate(trait = ifelse(enrich == "mvalue",
+                          str_replace(trait, "_C([0-9]+)$", "_M\\1"),
+                          trait)) %>%
+    select(-enrich) %>%
+    select(strain, sex, animal, condition, trait, minute, value)
+  
+  # Area under curve and other time summaries
+  auc <- area_under_curve(out, "minute") %>%
     select(strain, sex, animal, condition, trait, value)
+  
+  out <- out %>%
+    mutate(trait = paste(trait, minute, "18wk", sep = "_")) %>%
+    select(-minute)
+  
+  # These are harmonized columns and their names.
+  bind_rows(out, auc)
 }
 
-# Data Cleaning function 
-timepoint_data_cleaning <- function(dataset) {
-  dataset <- dataset %>%
-    # Rename labels as L0,L1,L2,L3,L4,L5,L6
-    mutate(Label = gsub("PARENT", "L0", Label),
-           Label = gsub("C13-label-1", "L1", Label),
-           Label = gsub("C13-label-2", "L2", Label),
-           Label = gsub("C13-label-3", "L3", Label),
-           Label = gsub("C13-label-4", "L4", Label),
-           Label = gsub("C13-label-5", "L5", Label),
-           Label = gsub("C13-label-6", "L6", Label))
-  
-  # Correcting format of Timepoints column
-  dataset %>%
-    mutate(
-      Timepoints = as.numeric(gsub("min", "", Timepoints)),
-           trait = paste(Name, Label, Timepoints, "wk18", sep = "_"))
+read_trait_sheet <- function(filename, sheet) {
+  traitname <- str_remove(sheet, "_labels")
+  read_excel(filename, sheet = sheet) %>%
+    rename(mouse_id = "Strains",
+           trait = "Label",
+           minute = "Timepoints",
+           value = "NA Corrected with zero",
+           run = "Sample Name") %>%
+    mutate(run = as.numeric(str_remove(str_remove(run, "-.*$"), "^run")),
+           minute = str_remove(minute, "min$"),
+           trait = paste0("C", str_remove(trait, "^.*\\-")),
+           trait = ifelse(trait == "PARENT", "C0", trait),
+           trait = paste(traitname, trait, sep = "_")) %>%
+    select(mouse_id, run, trait, minute, value) %>%
+    group_by(mouse_id, trait, minute) %>%
+    summarize(value = value[which.max(run)], .groups = "drop") %>%
+    ungroup()
 }
